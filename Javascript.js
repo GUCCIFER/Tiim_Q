42c00aff363ca78b3ff2b48e53f2c8310bbfb16e
Teet71 <teetsaar7@gmail.com>
2018-11-27 23:49:57 +0200

    nupp juures

--- Javascript.js
+++ Javascript.js
@@ -21,3 +21,22 @@ window.document.onkeydown = function(e) {
     document.getElementById('fade').style.display = 'none';
     lightBoxVideo.pause();
   }
+$(document).ready(function() {
+	$("#runner").on('mouseover', function(){
+    var offset = $(this).offset();
+    var goX = Math.random() < 0.5 ? -1 : 1;
+    var goY = Math.random() < 0.5 ? -1 : 1;
+    var targetSize = 70;
+    if(goX > 0 && offset.left + targetSize * goX + targetSize> 1200
+      || goX < 0 && offset.left + targetSize * goX  - targetSize < 0) {
+        goX = goX * (-1);
+    }
+    
+    if(goY > 0 && offset.top + targetSize * goY + targetSize> 700
+      || goY < 0 && offset.top + targetSize * goY - targetSize< 0) {
+        goY = goY * (-1);
+    }
+    $(this).css('top', offset.top + 70 * goY);
+    $(this).css('left', offset.left + 70 * goX);
+});
+});
